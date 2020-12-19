9cf60eaadd87332c4cab416866138e3cd579a2d0
Morlay <morlay.null@gmail.com>
2020-12-19 15:46:23 +0800

    patch 13751

--- .hack/patch.sh
+++ .hack/patch.sh
@@ -2,5 +2,6 @@
 
 set -eux;
 
-wget https://github.com/goharbor/harbor/pull/13788.patch
-git apply 13788.patch
+wget https://github.com/goharbor/harbor/pull/13751.patch && git apply 13751.patch
+
+wget https://github.com/goharbor/harbor/pull/13788.patch && git apply 13788.patch
