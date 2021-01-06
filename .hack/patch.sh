9d7d5b03aec52c7e2e534d0b65e3b190c53a58ba
Morlay <morlay.null@gmail.com>
2021-01-06 15:45:20 +0800

    updates

--- .hack/patch.sh
+++ .hack/patch.sh
@@ -2,8 +2,5 @@
 
 set -eux;
 
-# head content-type issue
-wget https://github.com/goharbor/harbor/pull/13805.patch && git apply 13805.patch
-
 wget https://github.com/goharbor/harbor/pull/13788.patch && git apply 13788.patch
 
