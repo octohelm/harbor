50d8bce0080a7d8c2447a93d629e1233cb06ef63
Morlay <morlay.null@gmail.com>
2021-06-29 10:01:16 +0800

    bump to 2.3.0

--- .hack/patch.sh
+++ .hack/patch.sh
@@ -2,6 +2,6 @@
 
 set -eux;
 
-wget -cO - https://github.com/goharbor/harbor/compare/release-2.2.0...morlay:release-2.2.0.patch > multi-arch.patch
+wget -cO - https://github.com/goharbor/harbor/compare/release-2.3.0...morlay:release-2.3.0.patch > multi-arch.patch
 git apply multi-arch.patch
 
